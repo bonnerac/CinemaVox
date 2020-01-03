4bc86d591247dd68c9fb91fb11f78bdafc7ff088
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-03 13:23:15 -0600

    added accent options

--- script.js
+++ script.js
@@ -1,21 +1,23 @@
+
 $(document).ready(function () {
     $(".card").hide()
-    console.log(screen.width);
     if (screen.width > 900) {
         $(".card").attr("class", "card horizontal")
     }
 
     $('select').formSelect();
-    // var selected_lang = 'en-us';
 
     var output_plot = "";
+    var accent = "";
 
-
+    console.log($("#select").val());
 
 
     $("#submit-btn").on("click", function (event) {
         event.preventDefault();
-        //$(".card").show()
+        console.log($("#select").val());
+
+        // $(".card").show()
         console.log(event);
         var movie = $("#textarea1").val().trim();
         console.log("Querying for movie:" + movie);
@@ -36,15 +38,47 @@ $(document).ready(function () {
                 console.log(response);
                 $("img").attr("src", response.Poster);
                 $(".card-title").text(response.Title + " (" + response.Year + ")");
-            }
 
+                if ($("#select").val() != 1 &&
+                    $("#select").val() != 2 &&
+                    $("#select").val() != 3 &&
+                    $("#select").val() != 4 &&
+                    $("#select").val() != 5) {
+                    alert("Please pick one accent!!!")
+                } else {
+
+                    if ($("#select").val() == 1) {
+                        accent = "en-au";
+                    }
+                    else if ($("#select").val() == 2) {
+                        accent = "en-ca";
+                    }
+                    else if ($("#select").val() == 3) {
+                        accent = "en-gb";
+                    }
+                    else if ($("#select").val() == 4) {
+                        accent = "en-in";
+                    }
+                    else if ($("#select").val() == 5) {
+                        accent = "en-us";
+                    }
+                    else {
+                        accent = "en-us"
+                    }
+                    console.log(accent);
+                }
+            }
         });
+        console.log(typeof ($("#select").val()));
+
 
 
     });
 
+
     $(".btn-floating").on("click", function () {
         console.log("working");
+        var queryURL_rss = "http://api.voicerss.org/?key=68d5397c419d4cbea5dfe35a4fc712c8&hl=en-us&src=" + output_plot;
         var queryURL_rss = "http://api.voicerss.org/?";
 
         $.ajax({
@@ -55,7 +89,7 @@ $(document).ready(function () {
             $.speech({
                 key: '56d46fda47404a73b8b4d0b4925d7eef',
                 src: output_plot,
-                hl: 'en-us',
+                hl: accent,
                 r: 0,
                 c: 'mp3',
                 f: '44khz_16bit_stereo',
@@ -66,5 +100,4 @@ $(document).ready(function () {
     })
 
 });
-
 
