028878bbf61dcce09b766d2caffd8ffb1a089b04
bonnerac <alex.c.bonner@gmail.com>
2020-01-03 10:04:00 -0600

    added drop down menus

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
-console.log(screen.width);
-if (screen.width > 900) {
-    $(".card").attr("class", "card horizontal")
-}
+$(document).ready(function () {
+    $(".card").hide()
+    if (screen.width > 900) {
+        $(".card").attr("class", "card horizontal")
+    }
+
+    $('select').formSelect();
 
 
 
-// var movie = "yol";
-var output_plot = "";
-$(".card").hide()
 
-//gets data from omdp and returns output 
-$("#submit-btn").on("click", function (event) {
-    event.preventDefault();
-    $(".card").show()
-    console.log(event);
-    var movie = $("#textarea1").val().trim();
-    console.log("Querying for movie:" + movie);
-    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=7e894ee5";
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        console.log("Got reponse:" + response);
-        output_plot = response.Plot;
-        $(".card-content").text(output_plot);
-        console.log(response);
-        $("img").attr("src", response.Poster);
-        $(".card-title").text(response.Title + " (" + response.Year + ")");
+    // var movie = "yol";
+    var output_plot = "";
 
+
+    //gets data from omdp and returns output 
+    $("#submit-btn").on("click", function (event) {
+        event.preventDefault();
+        // $(".card").show()
+        console.log(event);
+        var movie = $("#textarea1").val().trim();
+        console.log("Querying for movie:" + movie);
+        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=7e894ee5";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            if (response.Response == "False") {
+                // $(".card").hide()
+                alert("please enter a movie title")
+            }
+            else {
+                $(".card").show()
+                console.log("Got reponse:" + response);
+                output_plot = response.Plot;
+                $(".card-content").text(output_plot);
+                console.log(response);
+                $("img").attr("src", response.Poster);
+                $(".card-title").text(response.Title + " (" + response.Year + ")");
+            }
+
+
+
+        });
 
     });
+
+
 
 });
 
