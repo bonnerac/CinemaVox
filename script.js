5507d51c959499b238390a7b8ac1baa988609824
Zeliha Gencel <zelihagencel@gmail.com>
2020-01-03 11:02:49 -0600

    dummy

--- script.js
+++ script.js
@@ -1,79 +1,31 @@
+console.log(screen.width);
+if (screen.width > 900) {
+    $(".card").attr("class", "card horizontal")
+}
 
-$(document).ready(function () {
-    $(".card").hide()
-    if (screen.width > 900) {
-        $(".card").attr("class", "card horizontal")
-    }
+// var selected_lang = 'en-us';
 
-    $('select').formSelect();
+var output_plot = "";
+$(".card").hide()
 
-
-
-
-    // var movie = "yol";
-    var output_plot = "";
-
-
-    //gets data from omdp and returns output 
-    $("#submit-btn").on("click", function (event) {
-        event.preventDefault();
-        // $(".card").show()
-        console.log(event);
-        var movie = $("#textarea1").val().trim();
-        console.log("Querying for movie:" + movie);
-        var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=7e894ee5";
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-            if (response.Response == "False") {
-                // $(".card").hide()
-                alert("please enter a movie title")
-            }
-            else {
-                $(".card").show()
-                console.log("Got reponse:" + response);
-                output_plot = response.Plot;
-                $(".card-content").text(output_plot);
-                console.log(response);
-                $("img").attr("src", response.Poster);
-                $(".card-title").text(response.Title + " (" + response.Year + ")");
-            }
-
-
-
-        });
-
-
-// console.log(screen.width);
-// if (screen.width > 900) {
-//     $(".card").attr("class", "card horizontal")
-// }
-
-// var output_plot = "";
-// $(".card").hide()
-
-// $("#submit-btn").on("click", function (event) {
-//     event.preventDefault();
-//     $(".card").show()
-//     console.log(event);
-//     var movie = $("#textarea1").val().trim();
-//     console.log("Querying for movie:" + movie);
-//     var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=7e894ee5";
-//     $.ajax({
-//         url: queryURL,
-//         method: "GET"
-//     }).then(function (response) {
-//         console.log("Got reponse:" + response);
-//         output_plot = response.Plot;
-//         $(".card-content").text(output_plot);
-//         console.log(response);
-//         $("img").attr("src", response.Poster);
-//         $(".card-title").text(response.Title + " (" + response.Year + ")");
-
+$("#submit-btn").on("click", function (event) {
+    event.preventDefault();
+    $(".card").show()
+    console.log(event);
+    var movie = $("#textarea1").val().trim();
+    console.log("Querying for movie:" + movie);
+    var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=7e894ee5";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log("Got reponse:" + response);
+        output_plot = response.Plot;
+        $(".card-content").text(output_plot);
+        console.log(response);
+        $("img").attr("src", response.Poster);
+        $(".card-title").text(response.Title + " (" + response.Year + ")");
     });
-
-
 
 });
 
@@ -86,7 +38,6 @@ $(".btn-floating").on("click", function () {
         url: queryURL_rss,
         method: "GET"
     }).then(function (event) {
-        // console.log("Output is " + event);
         $.speech({
             key: '56d46fda47404a73b8b4d0b4925d7eef',
             src: output_plot,
@@ -96,6 +47,5 @@ $(".btn-floating").on("click", function () {
             f: '44khz_16bit_stereo',
             ssml: false
         });
-        console.log("Output is " + event);
     });
 })
